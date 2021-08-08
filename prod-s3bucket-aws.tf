993da358c64e27de4d2714f6f082e6fca833f67a
Dominik_Janiga <Dominik_Janiga@epam.com>
2021-08-08 21:22:19 +0300

    Web instances and elastic IP

--- prod-s3bucket-aws.tf
+++ prod-s3bucket-aws.tf
@@ -38,3 +38,23 @@ resource "aws_security_group" "prod_web" {
     "Terraform" : "true" 
   }
 }
+
+resource "aws_instance" "prod_web" {
+  ami 		= "ami-06f799cf417643393"
+  instance_type = "t2.nano"
+  vpc_security_group_ids = [
+    aws_security_group.prod_web.id
+  ]
+
+  tags = {
+    "Terraform" : "true" 
+  }
+}
+
+resource "aws_eip" "prod_web" {
+  instance = aws_instance.prod_web.id
+
+  tags = {
+    "Terraform" : "true" 
+  }
+}
